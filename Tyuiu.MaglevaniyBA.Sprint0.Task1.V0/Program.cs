c421323f2343f034c1cb0daf8a9155bf3827e78a
Grystniu <105716045+Grystniu@users.noreply.github.com>
2025-10-09 22:15:40 +0500

    Task1

---
+++ Tyuiu.MaglevaniyBA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.MaglevaniyBA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            String name = "Богдан";
+            int age = 23;
+
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет.");
+
+            Console.ReadKey();
+        }
+    }
+}
