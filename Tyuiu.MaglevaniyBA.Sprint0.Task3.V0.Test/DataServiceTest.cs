12dd0b6fd0680513ca40c1e9c2b84862ebdaa284
Grystniu <105716045+Grystniu@users.noreply.github.com>
2025-10-16 20:08:39 +0500

    Task3

---
+++ Tyuiu.MaglevaniyBA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.MaglevaniyBA.Sprint0.Task3.V0.Lib;
+
+
+namespace Tyuiu.MaglevaniyBA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.MaglevaniyBA.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MaglevaniyBA.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MaglevaniyBA.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e94daddc-7c83-4bb4-90ca-c178deb50d9d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
